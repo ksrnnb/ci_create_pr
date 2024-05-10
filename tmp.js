be9f66aee7f3f65d647dacec1a89195263b6702b
K. Okabe <48155865+ksrnnb@users.noreply.github.com>
2024-05-10 20:22:11 +0900

    chore: udpate action

--- tmp.js
+++ tmp.js
@@ -1,54 +1,82 @@
+const service = context.payload.inputs.service;
+const versionType = context.payload.inputs.versionType;
 
-const labelColor = '0052CD';
+console.log("service", service);
+console.log("versionType", versionType);
 
-const currentLabels = await github.rest.issues.listLabelsOnIssue({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    issue_number: context.payload.pull_request.number,
-})
-
-// filter by color to get services' label
-const currentServiceLabelNames = currentLabels.data.filter(label => label.color === labelColor).map(label => label.name);
-
+// paginate method get all data
+// https://docs.github.com/ja/rest/using-the-rest-api/using-pagination-in-the-rest-api?apiVersion=2022-11-28#example-using-the-octokitjs-pagination-method
 const releases = await github.paginate(github.rest.repos.listReleases, {
     owner: context.repo.owner,
     repo: context.repo.repo,
 });
 
-// TODO: filter currentServiceLabelNames を含む draft の PR が欲しい。
-const draftReleases = releases.data.filter(release => release.draft && currentServiceLabelNames.includes(release.name));
+const latestRelease = releases.find(release => !release.draft && release.name.includes(service));
+const draftRelease = releases.find(release => release.draft && service === release.name);
 
-const servicesToCreate = currentServiceLabelNames.filter(serviceName => draftReleases.find(release => release.name === serviceName) === undefined)
-const prTitle = context.payload.pull_request.title;
-const prNumber = context.payload.pull_request.number;
+console.log("latestRelease", latestRelease);
+console.log("draftRelease", draftRelease);
 
-console.log("draftReleases", draftReleases);
-console.log("currentServiceLabelNames", currentServiceLabelNames);
-console.log("servicesToCreate", servicesToCreate);
-console.log("prTitle", prTitle);
-console.log("prNumber", prNumber);
-return;
-// update draft release
-for (const draftRelease of draftReleases) {
-    const body = `${draftRelease.body}\n- ${prTitle} (#${prNumber})`;
-    await github.rest.repos.updateRelease({
-        owner,
-        repo,
-        release_id: draftRelease.id,
-        body
-    });
+if (!latestRelease && !draftRelease) {
+    console.error("Both latest release and draft release are not found.");
+    return;
 }
 
-// Create new draft
-for (const service of servicesToCreate) {
-    const body = `${prTitle} (#${prNumber})`;
+let newVersion;
+let oldTagName;
 
+if (!latestRelease) {
+    // if this is first release, base version is "0.0.0"
+    newVersion = nextVersion("0.0.0", versionType);
+} else {
+    const latestVersion = latestRelease.tag_name.split("/").at(2);
+    if (!latestVersion) {
+        throw Error(`latest version cannot be got from tag ${latestRelease.tag_name}`);
+    }
+    newVersion = nextVersion(latestVersion, versionType);
+}
+
+const newTagName = `microservices/${service}/${newVersion}`;
+
+if (!draftRelease) {
     await github.rest.repos.createRelease({
-        owner,
-        repo,
-        // tag_name: "draft-" + new Date().toISOString(), draft ならたぶんタグなしで作れる？ なければ自動生成？
-        name: service,
-        body: body,
-        draft: true
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        tag_name: newTagName,
+        name: newTagName,
+        body: `there is no difference between previous release ${latestRelease?.tag_name} and \`${newTagName}\``,
+        draft: false
     });
+    return;
+}
+
+await github.rest.repos.updateRelease({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    release_id: draftRelease.id,
+    tag_name: newTagName,
+    name: newTagName,
+    draft: false
+});
+
+const nextVersion = (currentVersion, versionType) => {
+    const versions = currentVersion.split(".");
+    if (versions.length !== 3) {
+        throw Error(`version has unexpected format ${currentVersion}, expected format is like "x.y.z"`);
+    }
+
+    const intVersions = versions.map(version => Number(version));
+    switch (versionType) {
+        case "major":
+            intVersions[0]++;
+            break;
+        case "minor":
+            intVersions[1]++;
+            break;
+        case "patch":
+            intVersions[2]++;
+            break;
+    }
+
+    return intVersions.join(".");
 }
